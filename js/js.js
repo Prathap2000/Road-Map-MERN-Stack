57f59d94072e5fd5021e1c042fb4c80889875005
PRATHAP J <83679951+Prathap2000@users.noreply.github.com>
2024-07-23 16:34:29 +0530

    var const let

--- js/js.js
+++ js/js.js
@@ -1,3 +1,15 @@
 console.log('" 1st javascript"');
-console.warn('its warnimg');
-console.error('its  error');
+// console.warn('its warnimg');
+// console.error('its  error');
+
+// we can use var,let  const to carete the variable 
+var lang="javascrip prathap";
+console.log(lang)
+// rules for variable name 
+// you can't start the namewith number,special symobl expect underscore(_) and dollar $
+// variable names are casein sensitive  
+//  the const is use for constant the value of perticular variabke 
+//  let is used for block level element 
+const arr=[20,30,40,60]
+arr.push=[50]
+console.log(arr)
